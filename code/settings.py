d51742962c0f6debdd33e7016474bc2a2ac7dce7
Hubert Nafalski <hubert.nafalski@gmail.com>
2022-08-21 19:53:31 +0200

    + build exe for Windows + added GOD_MODE + toggle cloud movement

--- code/settings.py
+++ code/settings.py
@@ -4,4 +4,17 @@ TILE_SIZE = 64
 SCREEN_HEIGHT = VERTICAL_TILE_NUMBER * TILE_SIZE
 SCREEN_WIDTH = 1200
 
+# playback backgroud music flag
 ENABLE_SOUND_ON_START = False
+
+# clouds on the sky moiving along with player
+MOVING_CLOUDS = False
+
+# immortal mode for debug
+GOD_MODE = False
+
+# how many levels are available on start
+START_MAX_LEVEL = 0
+
+# maximum amount of health on start
+START_MAX_HEALTH = 100
